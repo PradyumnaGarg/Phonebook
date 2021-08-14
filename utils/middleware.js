dcf1be67c98301295c312027aed8f53333e67724
PradyumnaGarg <pradumnagarg@hotmail.com>
2021-08-14 10:05:31 +0530

    Logger changes

--- utils/middleware.js
+++ utils/middleware.js
@@ -4,7 +4,7 @@ const requestLogger = (request, response, next) => {
     logger.info('Method: ', request.method);
     logger.info('Path:   ', request.path);
     logger.info('Body:   ', request.body);
-    logger.info('----');
+    logger.info('------');
     next();
 };
 
